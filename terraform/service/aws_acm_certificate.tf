282278e6d2c91b7e047d24d01089f37e0e5a1bec
yuyamita <y-mita@scope-inc.co.jp>
2022-04-08 19:41:16 +0900

    DNS設定修正

--- terraform/service/aws_acm_certificate.tf
+++ terraform/service/aws_acm_certificate.tf
@@ -1,5 +1,5 @@
 resource "aws_acm_certificate" "gwaretool" {
-    domain_name               = "gwaretool.com"
+    domain_name               = data.aws_route53_zone.gwaretool.name
     subject_alternative_names = []
     validation_method         = "DNS"
 }
