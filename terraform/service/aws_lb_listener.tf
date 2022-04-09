66df8476d6da1b4921170131e6cd7be2413355b7
yuyamita <y-mita@scope-inc.co.jp>
2022-04-09 09:48:17 +0900

    ssl関係修正

--- terraform/service/aws_lb_listener.tf
+++ terraform/service/aws_lb_listener.tf
@@ -7,3 +7,16 @@ resource "aws_lb_listener" "http" {
         type             = "forward"
     }
 }
+
+resource "aws_lb_listener" "https" {
+    load_balancer_arn = aws_lb.gwlb.arn
+    port              = "443"
+    protocol          = "HTTPS"
+    ssl_policy        = "ELBSecurityPolicy-2016-08"
+    certificate_arn   = aws_acm_certificate.gwaretool.arn
+
+    default_action {
+        target_group_arn = aws_lb_target_group.https.arn
+        type             = "forward"
+    }
+}
