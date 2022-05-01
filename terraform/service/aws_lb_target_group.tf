55ff7726b56911215a31df8a2ba8c351f84a2ff0
yuyamita <y-mita@scope-inc.co.jp>
2022-05-01 17:23:18 +0900

    ターゲットグループ修正

--- terraform/service/aws_lb_target_group.tf
+++ terraform/service/aws_lb_target_group.tf
@@ -24,7 +24,7 @@ resource "aws_lb_target_group" "https" {
         interval = 30
         path = "/"
         port = 443
-        protocol = "HTTP"
+        protocol = "HTTPS"
         timeout = 10
         healthy_threshold = 2
         unhealthy_threshold = 2
