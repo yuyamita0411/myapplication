0d17df5aa970f968131b1a148a6ecf7ef6fd9c84
yuyamita <y-mita@scope-inc.co.jp>
2022-05-01 16:17:39 +0900

    修正

--- terraform/service/aws_lb_target_group.tf
+++ terraform/service/aws_lb_target_group.tf
@@ -5,7 +5,7 @@ resource "aws_lb_target_group" "http" {
     vpc_id   = data.terraform_remote_state.vp.outputs.vpc_id
     health_check {
         interval = 30
-        path = "/login"
+        path = "/"
         port = 80
         protocol = "HTTP"
         timeout = 10
