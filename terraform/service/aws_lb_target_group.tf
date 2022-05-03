437d5a29c2af2810800d0719fc3428665460184c
yuyamita <y-mita@scope-inc.co.jp>
2022-05-03 10:44:13 +0900

    ターゲットグループ修正

--- terraform/service/aws_lb_target_group.tf
+++ terraform/service/aws_lb_target_group.tf
@@ -11,23 +11,6 @@ resource "aws_lb_target_group" "http" {
         timeout = 10
         healthy_threshold = 2
         unhealthy_threshold = 2
-        matcher = "200,302"
-    }
-}
-
-resource "aws_lb_target_group" "https" {
-    name     = "gware-https"
-    port     = 443
-    protocol = "HTTPS"
-    vpc_id   = data.terraform_remote_state.vp.outputs.vpc_id
-    health_check {
-        interval = 30
-        path = "/"
-        port = 443
-        protocol = "HTTPS"
-        timeout = 10
-        healthy_threshold = 2
-        unhealthy_threshold = 2
-        matcher = "200,302"
+        matcher = "200-302"
     }
 }
