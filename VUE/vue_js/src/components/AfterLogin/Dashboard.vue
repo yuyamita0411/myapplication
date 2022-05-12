3e29bb8d29ec1dfe6f38128d856d1b56e6dcdab1
yuyamita <y-mita@scope-inc.co.jp>
2022-05-12 21:21:44 +0900

    ダッシュボード作成

--- VUE/vue_js/src/components/AfterLogin/Dashboard.vue
+++ VUE/vue_js/src/components/AfterLogin/Dashboard.vue
@@ -1,32 +1,46 @@
 <template>
     <div class="shadowwrapper m-auto pl-2 pr-2 pl-lg-0 pr-lg-0">
         <div id="loadingarea" v-html="Loading"></div>
+
         <div class="contentwraper w-100 d-inline-block pt-3 pb-4 pl-0 pr-0 mt-3">
             <div :class="`articlewrapper col-12 m-auto p-3 ${loadstatus}`">
                 <h3 class="mainfontcolor newarticletitle col-12 pt-1 pb-1 mb-0 bg-transparent pl-0 pr-0">NEW!!</h3>
                 <div class="newestarticle d-flex w-100 bg-white pt-2 pb-2 pr-4 pl-4 mb-3">
-
+                    <article class="p-news-list__item js-inter fadeInLeft is-invasion">
+                        <a href="p-news-list__item%20js-inter%20fadeInLeft%20is-invasion">
+                        <div class="w-100">
+                            <p class="mb-0 font-weight-bold">{{TopNotification}}</p>
+                        </div></a>
+                    </article>
                 </div>
                 <div class="gridinside w-100 bg-white p-3">
-                    <img src="@/assets/clipicon.png" class="clipicon position-absolute">
-                    <div id="notificationarea"></div>
-
-                    <div class="d-inline-block w-100 mt-3">
-                        <div id="notificationpagenationarea" class="col-10 col-md-6 col-lg-4 d-flex m-auto">
-                            <article class="p-news-list__item js-inter fadeInLeft is-invasion mb-3 p-2">
-                                <div v-for="Nt in notificationarr" :key="Nt.id">
-                                    <div class="p-news-list__item__data pb-2" v-if="Nt != notificationarr[0] && Nt">
-                                        <p class="p-news-list__item__data__date mb-0"></p>
-                                        <div class="p-news-list__item__data__line"></div>
-                                        <a href="" class="p-news-list__item__data__category mb-0">{{Nt.title}}</a>
-                                    </div>
+                    <img class="clipicon position-absolute" src="@/assets/clipicon.png">
+                    <div id="notificationarea">
+                        <article class="p-news-list__item js-inter fadeInLeft is-invasion mb-3 p-2 position-relative" v-for="Nt in notificationarr" :key="Nt.id">
+                            <router-link :to="{path: Nt.notificationlink = Nt.notificationlink != null ? Nt.notificationlink : '/dashboard'}">
+                                <div class="p-news-list__item__data pb-2" v-if="Nt != notificationarr[0] && Nt">
+                                    <p class="p-news-list__item__data__date mb-0">
+                                    {{`
+                                        ${new Date(Nt.created_at).getFullYear()}-
+                                        ${new Date(Nt.created_at).getMonth()}-
+                                        ${new Date(Nt.created_at).getDate()}
+                                    `}}
+                                    </p>
+                                    <div class="p-news-list__item__data__line"></div>
+                                    <a href="" class="p-news-list__item__data__category mb-0">{{Nt.title}}</a>
                                 </div>
-                            </article>
+                            </router-link>
+                        </article>
+                    </div>
+                    <div class="d-inline-block w-100 mt-3">
+                        <div class="col-10 col-md-6 col-lg-4 d-flex m-auto" id="notificationpagenationarea">
+                            <p class="pagenationnum PageNow PageNationNum cursor w-100 text-center p-1" id="">1</p><input class="mt-0 mb-0 col-2" id="PageNationInput" name="PageNationInput" type="text" value="1">
                         </div>
                     </div>
                 </div>
             </div>
         </div>
+
     </div>
 </template>
 
