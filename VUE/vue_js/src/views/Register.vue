f0fa28d2d9d33afe09f5408b4b2d0397c1eb0a47
yuyamita <y-mita@scope-inc.co.jp>
2022-06-14 13:05:00 +0900

    APIエンドポイント修正

--- VUE/vue_js/src/views/Register.vue
+++ VUE/vue_js/src/views/Register.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="LoginmodalOutside p-2 p-md-0 text-center">
-        <div id="Loginmodal" class="LoginFormWrapper m-auto pt-5 pb-5 pr-3 pl-3 bg-white d-inline-block col-10 col-md-6 col-lg-4 position-relative">
+        <div id="Loginmodal" class="LoginFormWrapper m-auto pt-5 pb-5 pr-3 pl-3 bg-white d-inline-block col-12 col-md-6 col-lg-4 position-relative">
             <RegisterPage />
         </div>
     </div>
