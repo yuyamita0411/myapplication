6785b6533b2b41e12b8def75efdc810a13cad414
yuyamita <y-mita@scope-inc.co.jp>
2022-09-08 18:52:33 +0900

    修正

--- VUE/vue_js/src/http.ts
+++ VUE/vue_js/src/http.ts
@@ -1,5 +1,6 @@
 import axios, { AxiosInstance } from "axios";
 export const apiClient: AxiosInstance = axios.create({
+
     //本番反映の際はここを本番のドメインに変える
     //baseURL: "https://localhost:8081",
     baseURL: "https://gwaretool.com:8081",
